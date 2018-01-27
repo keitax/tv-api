3a6e33d53fc6f3f7bc3b807f5687a79fc7a1d7ba
keitax <keitax913@yahoo.co.jp>
2018-01-27 18:47:19 +0900

    Define the Post entity type

---
+++ entity.go
@@ -0,0 +1,38 @@
+package tvapi
+
+import "time"
+
+// Post is a entity type representing blog posts.
+type Post struct {
+	Key          string
+	Date         *time.Time
+	CreatedAt    *time.Time `db:"created_at"`
+	UpdatedAt    *time.Time `db:"updated_at"`
+	URLName      string     `db:"url_name"`
+	Title        string     `db:"title"`
+	Body         string     `db:"body"`
+	Labels       []string
+	NextPost     *Post
+	PreviousPost *Post
+}
+
+// PostList is type alias to sort post list.
+type PostList []*Post
+
+func (sp PostList) Len() int {
+	return len(sp)
+}
+
+func (sp PostList) Less(i, j int) bool {
+	if sp[i].Date == nil {
+		return false
+	}
+	if sp[j].Date == nil {
+		return true
+	}
+	return sp[i].Date.After(*sp[j].Date)
+}
+
+func (sp PostList) Swap(i, j int) {
+	sp[i], sp[j] = sp[j], sp[i]
+}
